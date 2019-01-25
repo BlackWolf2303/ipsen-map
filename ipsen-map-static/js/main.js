4faf51829bd3c1fe0414a1d3f9547946c244c816
Hai.Ho <hai.ho@it-consultis.com>
2019-01-25 17:49:36 +0700

    update

--- ipsen-map-static/js/main.js
+++ ipsen-map-static/js/main.js
@@ -1,86 +1,78 @@
 //finderjs
-var treeMap = document.getElementById("treeMap");
-var data = [
-  {
-    label: "Thearapeutic Area",
-    children: [
-      {
-        label: "Rare Disease",
-        children: [
-          {
-            label: "Publication"
-          },
-          {
-            label: "Key Guidelines & Others"
-          },
-          {
-            label: "Training"
-          },
-          {
-            label: "Clinical Trials"
-          },
-          {
-            label: "Congress Hightlights"
-          },
-          {
-            label: "Core content"
-          },
-          {
-            label: "Contact"
-          },
-          {
-            label: "Rare Diseases SharePoint"
-          }
-        ]
-      },
-      {
-        label: "Item 1B",
-        children: [
-          {
-            label: "Item 1B1"
-          },
-        ]
-      }
-    ]
-  },
 
-  {
-    label: "GMAS",
-    children: [ ]
-  },
-  {
-    label: "Useful SharePoint Links",
-    children: [ ]
-  },
-];
-var options = {};
-var f = finder(treeMap, data, options);
-f.on("leaf-selected", function(item) {
-  console.log("Leaf selected", item);
-  //Print out data which contain in lv3
-});
+// var data = (function() {
+  
+//   return json;
+//   var options = {};
+// })
+// ();
 
 
 //<=======JQUERY START =========>
+// jQuery(function($) {
+//   var treeMap = $("#treeMap");
+//   var screenMobile = $(window).width();
+  
+//   $.ajax({
+//     async: false,
+//     global: false,
+//     url: "/data/data.json",
+//     dataType: "json",
+//     success: function(data) {
+//       var f = finder(treeMap.get(0), data, {});
+//       f.on("leaf-selected", function(item) {
+//         console.log("Leaf selected", item);
+//         //Print out data which contain in lv3
+//       });
+//     }
+//   });
+//   //hidden tree level 1,2 for mobile
+
+
+//   if (screenMobile < 992) {
+//     $("#treeMap").on("click", ".fjs-item", function() {
+//       $(this)
+//         .parents(".fjs-col")
+//         .addClass("hidden");
+//       //change text of back-button
+//       var textBackButton = $(this).text();
+//       $("#category-title").text(textBackButton);
+//       $(".back-icon").toggleClass("hidden", textBackButton === "Select a category");
+//     });
+//   }
+
+//   //modal component
+// });
+//<=======JQUERY START =========>
 jQuery(function($) {
-  //hidden tree level 1,2 for mobile
-  var screenMobile = $(window).width();
+  
+  $.ajax({
+    async: false,
+    global: false,
+    url: "/data/data.json",
+    dataType: "json",
+    success: initFinderUI
+  });
 
-  if(screenMobile < 992) {
-    $("#treeMap").on('click',".fjs-item",function(){
-      $(this).parents(".fjs-col").addClass("hidden");
-      //change text of back-button 
-      var textBackButton = $(this).text();
-      $('#category-title').text(textBackButton);
-      //display icon back button only 
-      if($('#category-title').text()!=='Select a category'){       
-          $('.back-icon').removeClass('hidden');
-        }else {
-          $('.back-icon').addClass('hidden');
-        }
+  function initFinderUI(data) {
+    var treeMap = $("#treeMap");
+    var screenMobile = $(window).width();
+    var f = finder(treeMap.get(0), data, {});
+    f.on("leaf-selected", function(item) {
+      console.log("Leaf selected", item);
+      //Print out data which contain in lv3
     });
+    if (screenMobile < 992) {
+      $("#treeMap").on("click", ".fjs-item", function() {
+        $(this)
+          .parents(".fjs-col")
+          .addClass("hidden");
+        //change text of back-button
+        var textBackButton = $(this).text();
+        $("#category-title").text(textBackButton);
+        $(".back-icon").toggleClass("hidden", textBackButton === "Select a category");
+      });
+    }
   }
-
-    //modal component
-
+  //modal component
 });
